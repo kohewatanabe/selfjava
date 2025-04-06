6b342125c6c0833ec3da04d79446d584a9dfc225
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-07 07:19:57 +0900

    6

--- src/chap_2/s_2_2.java
+++ src/chap_2/s_2_2.java
@@ -3,9 +3,9 @@ package chap_2;
 public class s_2_2 {
     public static void main(String[] args) {
         var i = 108;
-        var str = "hello world";
+        var x = 1.4142e5;
         System.out.println(i);
-        System.out.println(str);
+        System.out.println(x);
     }
 
 }
