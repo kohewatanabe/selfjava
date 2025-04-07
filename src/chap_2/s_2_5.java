9ab313f9396217580a8ff82fb541dd0f479eb775
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-08 08:32:08 +0900

    10

--- src/chap_2/s_2_5.java
+++ src/chap_2/s_2_5.java
@@ -1,14 +1,25 @@
 package chap_2;
-import java.io.File;
 
 public class s_2_5 {
     public static void main(String[] args) {
-        Object o = 10;
-        o = "Hoge";
-        o = new File("C:\\data");
-        //Objectには様々なデータが入る(データの種類によらない)。
+        int[] list1 = new int[5];
+        int[] list2 = {1, 2, 3, 4, 5};
+        String[] list3 = {
+            "すずめの子 そこのけそこのけ お馬が通る",
+            "目には青葉 山ホトトギス 初がつお",
+            "朝顔に つるべとられて もらい水",
+        };
 
-        System.out.println(o);
+        var list4 = new int[5];
+        var list5 = new int[] {1, 2, 3, 4, 5};
+
+
+        System.out.println(list1);
+        System.out.println(list2);
+        System.out.println(list3);
+        System.out.println(list4);
+        System.out.println(list5);
+
     }
 
 }
