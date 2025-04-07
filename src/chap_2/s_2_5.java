c8fad7988adc432253ef7b8f27379069b044546d
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-08 08:17:55 +0900

    9

--- src/chap_2/s_2_5.java
+++ src/chap_2/s_2_5.java
@@ -3,10 +3,12 @@ import java.io.File;
 
 public class s_2_5 {
     public static void main(String[] args) {
-        File f = null;
-        String name = f.getName();
+        Object o = 10;
+        o = "Hoge";
+        o = new File("C:\\data");
+        //Objectには様々なデータが入る(データの種類によらない)。
 
-        System.out.println(name);
+        System.out.println(o);
     }
 
 }
