b3e2806e883269ffd2a36543721b5a9e7f23be5d
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-08 09:06:14 +0900

    11

--- src/chap_2/s_2_5.java
+++ src/chap_2/s_2_5.java
@@ -13,12 +13,37 @@ public class s_2_5 {
         var list4 = new int[5];
         var list5 = new int[] {1, 2, 3, 4, 5};
 
+        list1[0] = 15;
 
-        System.out.println(list1);
-        System.out.println(list2);
+        int[][] list6 = new int[3][5];
+        int[][] list7 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9},
+        };
+
+        var list8 = new int[3][];//1次元の配列で宣言しても同じなのではと思ったが、二次元の配列で宣言しないと、配列を入れ子に出来ない(配列の要素として配列を持てるデータ構造にならない)。
+        list8[0] = new int[2];
+        list8[1] = new int[3];
+        list8[2] = new int[4];
+
+        var list9 = new int[][] {
+            {1, 2},
+            {3, 4, 5, 6},
+            {7, 8, 9},
+        };
+        
+
+
+        System.out.println(list1[4]);
+        System.out.println(list2.length);
         System.out.println(list3);
         System.out.println(list4);
         System.out.println(list5);
+        System.out.println(list6[0][2]);
+        System.out.println(list7[0][1]);
+        System.out.println(list8[0][1]);
+        System.out.println(list9[0][1]);
 
     }
 
