c59d503e7456ff93bb2d08986a495278843dbe20
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-10 08:04:42 +0900

    19

--- src/chap_3/s_3_3.java
+++ src/chap_3/s_3_3.java
@@ -1,5 +1,8 @@
 package chap_3;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class s_3_3 {
     public static void main() {
         StringBuilder builder1 = new StringBuilder("あいう");//StringクラスとStringBUilderクラスは明確に異なる。Stringクラスは不可変文字列を生成するクラスであり、StringBuilderクラスは可変文字列を生成するクラスである。
@@ -7,6 +10,28 @@ public class s_3_3 {
 
         System.out.println(builder1 == builder2);
         System.out.println(builder1.equals(builder2));
+
+        var bd1 = new BigDecimal("0.2");
+        var bd2 = new BigDecimal("3");
+        var bd3 = new BigDecimal("0.6");
+
+        System.out.println(bd1.multiply(bd2).compareTo(bd3));
+        
+        final double EPSILON = 0.00001;
+        var x = 0.2 * 3;
+        var y = 0.6;
+
+        System.out.println(Math.abs(x - y) < EPSILON);
+
+        var data1 = new String[] {"あいうえお"};
+        var data2 = new String[] {"あいうえお"};
+
+        System.out.println(data1 == data2);
+        System.out.println(data1.equals(data2));
+
+        System.out.println(Arrays.equals(data1, data2));
+
+
     }
 
 }
