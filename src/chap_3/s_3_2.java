985c012c770d561551ba5483e348161c764dd776
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-10 07:15:21 +0900

    16

--- src/chap_3/s_3_2.java
+++ src/chap_3/s_3_2.java
@@ -1,5 +1,21 @@
 package chap_3;
 
 public class s_3_2 {
+    public static void main(String[] args) {
 
+        var x = 1;
+        var y = x;
+        x += 10;
+        System.out.println(x);
+        System.out.println(y);
+
+        var builder1 = new StringBuilder("あいう");
+        var builder2 = builder1;
+
+        builder1.append("えお");
+
+        System.out.println(builder1);
+        System.out.println(builder2);
+    
+    }
 }
