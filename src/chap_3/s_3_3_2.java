8c8e9f82c5c2e2464048498ee57025653d981895
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-11 07:58:02 +0900

    21

---
+++ src/chap_3/s_3_3_2.java
@@ -0,0 +1,18 @@
+package chap_3;
+
+public class s_3_3_2 {
+    public static void main(String[] args) {
+        var age = 30;
+        System.out.println(age >= 20 ? "おとな" : "こども");
+
+        String value = "あいう";
+        if (value == null) {
+            System.out.println("えお");
+        } else {
+            System.out.println(value);
+        }
+
+
+    }
+
+}
