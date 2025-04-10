99edc99420c4bbd735b6da2131b1528019a72a1c
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-11 08:05:34 +0900

    22

--- src/chap_3/s_3_3_2.java
+++ src/chap_3/s_3_3_2.java
@@ -5,7 +5,7 @@ public class s_3_3_2 {
         var age = 30;
         System.out.println(age >= 20 ? "おとな" : "こども");
 
-        String value = "あいう";
+        String value = new String("あいう");// クラスのインスタンス化はクラス(引数)でインスタンス化。
         if (value == null) {
             System.out.println("えお");
         } else {
