f20bdb531a29d93a302c39717d95e86d16fd5447
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-12 12:51:02 +0900

    23

--- src/chap_3/s_3_4.java
+++ src/chap_3/s_3_4.java
@@ -1,5 +1,12 @@
 package chap_3;
 
 public class s_3_4 {
+    public static void main(String[] args) {
+        String str = null;
+
+        if (str != null && str.startsWith(("https://"))) {
+            System.out.println("「https://~」ではじまります。");// エラーが出てしまうと途中でプログラムが止まってしまうので出さないようにしている。
+        }
+    }
 
 }
