4701e399a2d9da7075d7f00a20c17dfcaa1ad848
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-13 10:30:11 +0900

    28

---
+++ src/chap_4/s_4_1_4.java
@@ -0,0 +1,15 @@
+package chap_4;
+
+public class s_4_1_4 {
+    public static void main(String[] args) {
+        int test = 80;
+        if (test >= 90) {
+            System.out.println("優");
+        } else if (test >= 70){
+            System.out.println("可");
+        } else {
+            System.out.println("不可");
+        }
+    }
+
+}
