2dea8d33b4818df9a0d97147c09a122227761038
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-16 07:35:48 +0900

    50

---
+++ src/chap_7/constructor/ConstBasic.java
@@ -0,0 +1,9 @@
+package chap_7.constructor;
+
+public class ConstBasic {
+    public static void main(String[] args) {
+        Person person = new Person("山田太郎", 30);
+        System.out.println(person.show());
+    }
+
+}
