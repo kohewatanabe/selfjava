17c2ace19398cf7ec6bdc165f77519f6eda7b74c
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-16 19:55:29 +0900

    52

---
+++ src/chap_7/constructor/factory/MainPCircle.java
@@ -0,0 +1,9 @@
+package chap_7.constructor.factory;
+
+public class MainPCircle {
+    static void main(String[] args) {
+        PCircle pCircle = new PCircle();
+        System.out.println(pCircle.getArea());
+    }
+
+}
---
+++ src/chap_7/constructor/factory/PCircle.java
@@ -0,0 +1,11 @@
+package chap_7.constructor.factory;
+
+public class PCircle {
+    private double radius = 1.0;
+    
+
+    public double getArea() {
+     return this.radius * this.radius * 3.14;
+    }
+
+}
