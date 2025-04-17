7f33719fa682b683bb0049875178b1ba282511e6
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-18 07:36:05 +0900

    58

---
+++ src/chap_7/practice/getAverageClient.java
@@ -0,0 +1,8 @@
+package chap_7.practice;
+
+public class getAverageClient {
+    public static void main(String[] args) {
+        System.out.println(getAverage.getaverage(1,2,3,4,5,6,7,8,9,10));
+
+    }
+}
