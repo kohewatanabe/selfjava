4a993d8038ea1279707bcb8fe270f1623adff449
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-20 16:40:50 +0900

    69

--- src/chap8/construct/MyChild.java
+++ src/chap8/construct/MyChild.java
@@ -4,7 +4,7 @@ public class  MyChild extends MyParent{
     public MyChild(String name) {
         //ここで親のコンストラクタに引数を渡して初期化しないといけない。親のコンストラクタが引数を持たない場合はデフォルトコンストラクタ(引数なしのコンストラクタ)が呼び出されて
         //勝手に初期化されるが、親が引数ありのコンストラクタを持つ場合は、親クラス由来のインスタンスの初期化に失敗し、エラーを吐く。
-        super(name);
+        super(name);//コンストラクタ呼び出しに使うsuperは、親クラスそのものを指す。
         System.out.printf("子の%sです。\n", name);
     }
 }
