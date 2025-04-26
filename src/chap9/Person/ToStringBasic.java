75eb3536dbdf10dbe6e97c6bbfd2633875827364
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-26 13:39:02 +0900

    76

---
+++ src/chap9/Person/ToStringBasic.java
@@ -0,0 +1,10 @@
+package chap9.Person;
+
+public class ToStringBasic {
+    public static void main(String[] args) {
+        Person p = new Person("太郎", "山田");
+
+        System.out.println(p);
+
+    }
+}
