813f3332922de7659a27f5f666fdbdba3c8e6ee5
kohewatanabe <kohei.watanabe.1203@gmail.com>
2025-04-29 13:42:51 +0900

    91

--- src/chap9/record/Item.java
+++ src/chap9/record/Item.java
@@ -1,3 +1,8 @@
 package chap9.record;
 
-public record Item(String name, int price) {}
+public record Item(String name, int price) {
+    
+    public void print() {
+        System.out.printf("%-7s %d yen\n", this.name, this.price);
+    }
+}
--- src/chap9/record/Shop.java
+++ src/chap9/record/Shop.java
@@ -3,10 +3,24 @@ package chap9.record;
 public class Shop {
     public static void main(String[] args) {
         Item a = new Item("burger", 100);
+        Item b = new Item("burger", 100);
+        Item c = new Item("potato", 150);
+        Item[] item = {
+            new Item("burger", 100),
+            new Item("potato", 150),
+            new Item("shake", 120)
+        };
 
         System.out.println(a);
 
         System.out.println(a.name());//getterを呼び出してprivateフィールドにアクセス。
         System.out.println(a.price());
+
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+
+        for(Item i : item) {
+            i.print();
+        }
     }
 }
